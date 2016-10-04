6682f745661eac794e6843c2ae24e9b05afa5702
MARY BETH CLARK <marybeth.clark@icloud.com>
2016-10-04 16:55:08 -0400

    add essential files outside of app directory, cleaned up code

--- app/view1/managebusinessservices.js
+++ app/view1/managebusinessservices.js
@@ -1,5 +1,3 @@
-
-// working code below ******************************
 'use strict';
 
  var biz = angular.module('myApp.manageBusinessServices', ['ngRoute'])
@@ -15,29 +13,9 @@
 //
 // }]);
 
-// **********************************************************
-// ***************   TEST CODE BELOW ************************
-// **********************************************************
-
-
-// var biz = angular.module('myApp.view1', ['ngRoute']);
-//
-// biz.controller('View1Ctrl', function($scope, $http) {
-//   $http.get("assets/js/fakeData.json").then(function(response){
-//       $scope.businessServices = response.data.records;
-//   });
-// });
-
-
-
-// ************* A DIFFERENT VERSION BELOW ***************
 // code source, the tutorial:Using AngularJS service | Reading JSON file http://blog.hfarazm.com/angularjs-service/
-//
-// var biz = angular.module('myApp.view1', ['ngRoute']);
-//
 biz.controller('manageBusinessServicesCtrl', ['$scope','$http',function($scope, $http) {
   $http.get("assets/js/fakeData.json").then(function(data){
-      $scope.businessServices = data.data;
-      // businessServices is a variable, or is it a property?
+    $scope.businessServices = data.data;
   });
 }]);
--- app/view1/view1.js
+++ app/view1/view1.js
@@ -1,5 +1,4 @@
 
-// working code below ******************************
 'use strict';
 
  var biz = angular.module('myApp.view1', ['ngRoute'])
@@ -11,33 +10,8 @@
   });
 }])
 
-// .controller('View1Ctrl', [function() {
-//
-// }]);
-
-// **********************************************************
-// ***************   TEST CODE BELOW ************************
-// **********************************************************
-
-
-// var biz = angular.module('myApp.view1', ['ngRoute']);
-//
-// biz.controller('View1Ctrl', function($scope, $http) {
-//   $http.get("assets/js/fakeData.json").then(function(response){
-//       $scope.businessServices = response.data.records;
-//   });
-// });
-
-
-
-// ************* A DIFFERENT VERSION BELOW ***************
-// code source, the tutorial:Using AngularJS service | Reading JSON file http://blog.hfarazm.com/angularjs-service/
-//
-// var biz = angular.module('myApp.view1', ['ngRoute']);
-//
 biz.controller('View1Ctrl', ['$scope','$http',function($scope, $http) {
   $http.get("assets/js/fakeData.json").then(function(data){
       $scope.businessServices = data.data;
-      // businessServices is a variable, or is it a property?
   });
 }]);
