48a1c960d0162b004243acc99f3f5cbe50032ac9
MARY BETH CLARK <marybeth.clark@icloud.com>
2016-10-13 15:58:18 -0400

    add custom directive for input validation

--- app/view3/addedge.js
+++ app/view3/addedge.js
@@ -9,6 +9,63 @@ angular.module('myApp.addEdge', ['ngRoute'])
   });
 }])
 
-.controller('addEdgeCtrl', [function() {
 
-}]);
+.controller('addEdgeCtrl', function($scope) {
+  // function to submit the form after all validation has occurred
+$scope.submitForm = function(isValid){
+   // check to make sure the form is completely valid
+   if (isValid){
+  alert('Success!')
+   }
+ };
+
+})
+// 0000000000000000000000000000000000
+.directive('nksOnlyNumber', function () {
+            return {
+                restrict: 'EA',
+                require: 'ngModel',
+                link: function (scope, element, attrs, ngModel) {
+                     scope.$watch(attrs.ngModel, function(newValue, oldValue) {
+                          var spiltArray = String(newValue).split("");
+
+                          if(attrs.allowNegative == "false") {
+                            if(spiltArray[0] == '-') {
+                              newValue = newValue.replace("-", "");
+                              ngModel.$setViewValue(newValue);
+                              ngModel.$render();
+                            }
+                          }
+
+                          if(attrs.allowDecimal == "false") {
+                              newValue = parseInt(newValue);
+                              ngModel.$setViewValue(newValue);
+                              ngModel.$render();
+                          }
+
+                          if(attrs.allowDecimal != "false") {
+                            if(attrs.decimalUpto) {
+                               var n = String(newValue).split(".");
+                               if(n[1]) {
+                                  var n2 = n[1].slice(0, attrs.decimalUpto);
+                                  newValue = [n[0], n2].join(".");
+                                  ngModel.$setViewValue(newValue);
+                                  ngModel.$render();
+                               }
+                            }
+                          }
+
+
+                          if (spiltArray.length === 0) return;
+                          if (spiltArray.length === 1 && (spiltArray[0] == '-' || spiltArray[0] === '.' )) return;
+                          if (spiltArray.length === 2 && newValue === '-.') return;
+
+                        /*Check it is number or not.*/
+                        if (isNaN(newValue)) {
+                          ngModel.$setViewValue(oldValue);
+                          ngModel.$render();
+                        }
+                    });
+                }
+            };
+        });
