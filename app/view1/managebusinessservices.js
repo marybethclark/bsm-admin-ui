d94c98dbe0e31a7608e66e3f204d7a57f0b94d7a
MARY BETH CLARK <marybeth.clark@icloud.com>
2016-11-10 15:43:24 -0500

    write function to flatten nested levels of json data

--- app/view1/managebusinessservices.js
+++ app/view1/managebusinessservices.js
@@ -1,17 +1,35 @@
-'use strict';
+"use strict";
 
- var biz = angular.module('myApp.manageBusinessServices', ['ngRoute'])
+var biz = angular.module("myApp.manageBusinessServices", ["ngRoute"])
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/managebusinessservices', {
-    templateUrl: 'view1/managebusinessservices.html',
-    controller: 'manageBusinessServicesCtrl'
-  });
-}])
+.config(["$routeProvider", function($routeProvider) {
+    $routeProvider.when("/managebusinessservices", {
+        templateUrl: "view1/managebusinessservices.html",
+        controller: "manageBusinessServicesCtrl"
+    });
+}]);
 
 // code source, the tutorial:Using AngularJS service | Reading JSON file http://blog.hfarazm.com/angularjs-service/
-biz.controller('manageBusinessServicesCtrl', ['$scope','$http',function($scope, $http) {
-  $http.get("assets/js/fakeData.json").then(function(response){
-    $scope.businessServices = response.data;
-  });
-}]);
+
+biz.controller("manageBusinessServicesCtrl",
+["$scope", "$http", function($scope, $http) {
+    $http.get("assets/js/fakeData.json").then(
+      function(response){
+        var businessServicesData = response.data;
+
+
+        var flatten = function(h) {
+            var flattenedArray = [];
+            for (var i = 0; i < h.length; i++) {
+                var currentElement = h[i];
+                flattenedArray.push(currentElement.name);
+               flattenedArray = flattenedArray.concat(flatten(currentElement.childName));
+            }
+            return flattenedArray;
+        };
+        var result = flatten(businessServicesData);
+        console.log(result);
+        $scope.businessServices = result;
+    });
+  }
+]);
